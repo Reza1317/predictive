64b264cd4f6768687fd799dd6832b66a9bf6ae56
Reza H <reza1317@gmail.com>
2015-11-16 10:14:24 +0000

    index.js modified

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {res.sendfile(__dirname + '/index.html');});
-app.listen(port, function() {console.log("Node app is running at localhost:" + app.get('port'))});
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
